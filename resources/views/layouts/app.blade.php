2e8991ebb4899c6321a81862b95f4802beb3121d
Yusnardo Tendio <yus21newbie@gmail.com>
2018-07-31 16:57:00 +0700

    tes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="/css/master.css" rel="stylesheet">
-    <link href="/sass/app.scss" rel="stylesheet">
+    <link href="../../css/master.css" rel="stylesheet">
+    <link href="../../sass/app.scss" rel="stylesheet">
 </head>
 
 <body>
@@ -149,8 +149,8 @@
         <hr class="hr-primary">
     </div>
     <!-- Scripts -->
-    <script src="js/app.js/"></script>
-    <script src="public/js/bootstrap.js/"></script>
+    <script src="../../js/app.js/"></script>
+    <script src="../../public/js/bootstrap.js/"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
