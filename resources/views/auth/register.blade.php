e5c20c447b60f426d021deba75af9bb18df089e6
Yusnardo Tendio <yus21newbie@gmail.com>
2018-08-01 22:54:21 +0700

    fix display

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -41,7 +41,7 @@
                         <label for="password" class="col-md-12 control-label">Password</label>
 
                         <div class="col-md-10">
-                            <input id="password" type="password" class="form-control is-invalid" name="password" required>
+                            <input id="password" type="password" class="form-control" name="password" required>
 
                             @if ($errors->has('password'))
                                 <span class="invalid-feedback">
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,20 @@ Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
 Route::get('auth/activate', 'Auth\ActivationController@activate')->name('auth.activate');
 
 Route::get('/home', 'HomeController@index');
