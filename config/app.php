b5ff8bdcf43d6b524d19b23695b7682494d11fc3
Yusnardo Tendio <yus21newbie@gmail.com>
2018-08-02 13:34:39 +0700

    a

--- config/app.php
+++ config/app.php
@@ -121,7 +121,9 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
+    'log' => 'errorlog',
+
+    // 'log' => env('APP_LOG', 'single'),
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
